50f4a206709bffc9aa7adb0a646dde046931bb61
tsthakur123 <singh121tarun@gmail.com>
2024-08-14 19:06:10 +0530

    minor css fix

--- components/Card.tsx
+++ components/Card.tsx
@@ -26,7 +26,7 @@ const Card: React.FC<CardProps> = ({
       <CardBody
         className={`bg-gray-50 relative group/card shadow-lg dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] border-black/[0.1] ${width} ${height} rounded-xl p-6 border flex flex-col justify-between`}
       >
-        <CardItem translateZ="100" className="w-full mt-4 flex justify-center">
+        <CardItem translateZ="100" className="w-full mt-4 flex">
           <Image
             src={image}
             height={imgH}
@@ -39,11 +39,11 @@ const Card: React.FC<CardProps> = ({
           <CardItem
             translateZ={40}
             as="h3"
-            className="text-2xl font-semibold text-center"
+            className="text-2xl font-semibold"
           >
             {title}
           </CardItem>
-          <CardItem translateZ={20} as="p" className="text-xs text-center">
+          <CardItem translateZ={20} as="p" className="text-xs">
             {content}
           </CardItem>
         </div>
